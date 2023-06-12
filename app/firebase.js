8d48415089a7fac5a1a5af6747d81d504ab85231
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-12 02:02:26 -0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -5,7 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase
 const firebaseConfig = {
   // Paste your firebase config here
 
-  
+   apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
+  authDomain: "sabores-house.firebaseapp.com",
+  projectId: "sabores-house",
+  storageBucket: "sabores-house.appspot.com",
+  messagingSenderId: "42245565592",
+  appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
+  measurementId: "G-87B5870XV6"
 };
 
 // Initialize Firebase
