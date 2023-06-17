da63fca0247d5295b1c323e19ba90d59953105f4
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-16 21:01:15 -0600

    Update postList.js

--- app/postList.js
+++ app/postList.js
@@ -1,20 +1,51 @@
-const postList = document.querySelector(".posts");
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-export const setupPosts = (data) => {
-  if (data.length) {
-    let html = "";
-    data.forEach((doc) => {
-      const post = doc.data();
-      const li = `
-      <li class="list-group-item list-group-item-action">
-        <h5>${post.title}</h5>
-        <p>${post.content}</p>
-      </li>
-    `;
-      html += li;
-    });
-    postList.innerHTML = html;
-  } else {
-    postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
+const firestore = getFirestore(); // Obtener una instancia de Firestore
+
+// Función para generar un elemento <li> de publicación
+const createPostElement = (post) => {
+  const li = document.createElement("li");
+  li.classList.add("list-group-item", "list-group-item-action");
+
+  const title = document.createElement("h5");
+  title.textContent = post.title;
+
+  const content = document.createElement("p");
+  content.textContent = post.content;
+
+  li.appendChild(title);
+  li.appendChild(content);
+
+  return li;
+};
+
+export const setupPosts = async () => {
+  const postListElement = document.querySelector(".posts");
+
+  try {
+    const querySnapshot = await getDocs(collection(firestore, "posts"));
+
+    if (querySnapshot.empty) {
+      const messageElement = document.createElement("h4");
+      messageElement.classList.add("text-white");
+      messageElement.textContent = "No posts found";
+      postListElement.appendChild(messageElement);
+    } else {
+      const postElements = [];
+      querySnapshot.forEach((doc) => {
+        const post = doc.data();
+        postElements.push(createPostElement(post));
+      });
+
+      postElements.forEach((postElement) => {
+        postListElement.appendChild(postElement);
+      });
+    }
+  } catch (error) {
+    console.error("Error retrieving posts:", error);
+    const messageElement = document.createElement("h4");
+    messageElement.classList.add("text-white");
+    messageElement.textContent = "Error retrieving posts";
+    postListElement.appendChild(messageElement);
   }
 };
