cc32d5e259d66cb48dc5fdc8dee7095178d4c788
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 20:54:16 -0600

    Update signinForm.js

--- app/signinForm.js
+++ app/signinForm.js
@@ -1,4 +1,5 @@
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+
+
 import { auth } from "./firebase.js";
 import { showMessage } from "./showMessage.js";
 
