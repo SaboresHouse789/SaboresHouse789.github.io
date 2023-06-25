fa77c8d7227b242990eefefbbaa1acabfaebe9f1
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 02:03:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
   authDomain: "sabores-house.firebaseapp.com",
   databaseURL: "https://sabores-house-default-rtdb.firebaseio.com",
@@ -15,6 +17,7 @@ const firebaseConfig = {
   messagingSenderId: "42245565592",
   appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
   measurementId: "G-87B5870XV6"
+};
 };
 
 // Initialize Firebase
