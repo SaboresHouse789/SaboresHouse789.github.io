39953c5c0a4b8972601ddd962ba2e7cbae1b2685
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 20:20:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { loginCheck } from "./app/loginCheck.js";
+import { setupPosts } from "./app/postList.js";
+
+import './app/signupForm.js';
+import './app/signinForm.js';
+import './app/googleLogin.js';
+import './app/facebookLogin.js';
+import './app/githubLogin.js';
+import './app/logout.js';
+import './app/postList.js';
 // Código existente...
 
 // Después de que se haya completado el registro exitosamente
@@ -36,17 +47,7 @@ typewriter
   .deleteChars(10)
   .start();
 
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
-import { loginCheck } from "./app/loginCheck.js";
-import { setupPosts } from "./app/postList.js";
 
-import './app/signupForm.js';
-import './app/signinForm.js';
-import './app/googleLogin.js';
-import './app/facebookLogin.js';
-import './app/githubLogin.js';
-import './app/logout.js';
-import './app/postList.js';
 
 // list for auth state changes
 onAuthStateChanged(auth, async (user) => {
