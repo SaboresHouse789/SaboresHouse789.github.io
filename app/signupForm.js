21bbf60c4b95b9ab4a046345d685d9181801541f
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 19:18:49 -0600

    Update signupForm.js
    cambios Felipe 1

--- app/signupForm.js
+++ app/signupForm.js
@@ -1,18 +1,27 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
-import { auth } from "./firebase.js";
-import { showMessage } from "./showMessage.js";
-
+import { createUserWhitEmailAndPasswword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import{ auth } from './'firebase.js'
 const signUpForm = document.querySelector("#signup-form");
 
 signUpForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
+  e.preventDefault()
+  
   const email = signUpForm["signup-email"].value;
   const password = signUpForm["signup-password"].value;
 
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-    console.log(userCredential)
+  console.log(email, password)
 
+  try { 
+   const userCredentials = await createUserWhitEmailAndPassword(auth, email, password)
+console.log(userCredentials) 
+  } catch(error) {
+    console.log(error)
+   
+  
+
+
+
+
+  
     // Close the signup modal
     const signupModal = document.querySelector('#signupModal');
     const modal = bootstrap.Modal.getInstance(signupModal);
