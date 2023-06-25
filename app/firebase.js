2ac8a0dae22c570a7d4c17d7e0812857d3650618
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 02:19:39 -0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,30 +1,26 @@
-export { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-export { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
-export { auth } from './app/firebase.js';
-export { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-export { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "./app/firebase.js";
+import { getAnalytics } from "./firebase/analytics";
+import { auth } from './app/firebase.js';
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Paste your firebase config here
-apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
-
-    authDomain: "sabores-house.firebaseapp.com",
-
-    databaseURL: "https://sabores-house-default-rtdb.firebaseio.com",
-
-    projectId: "sabores-house",
-
-    storageBucket: "sabores-house.appspot.com",
-
-    messagingSenderId: "42245565592",
-
-    appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
-
-    measurementId: "G-87B5870XV6"
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
+  authDomain: "sabores-house.firebaseapp.com",
+  databaseURL: "https://sabores-house-default-rtdb.firebaseio.com",
+  projectId: "sabores-house",
+  storageBucket: "sabores-house.appspot.com",
+  messagingSenderId: "42245565592",
+  appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
+  measurementId: "G-87B5870XV6"
 };
 
-// Initialize Firebase
-  const app = initializeApp(firebaseConfig);
 
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
