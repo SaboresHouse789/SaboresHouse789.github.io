b0759b753befffa1a1186c11f33530f4aabe2189
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 00:33:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 import { auth } from './app/firebase.js';
 
 import { setupPosts } from "./app/postList.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 import './app/signupForm.js';
 import './app/signinForm.js';
