88f075d3ceece32ebed2057b701e10a050853944
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 02:13:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "./app/firebase.js";
 import { getAnalytics } from "./firebase/analytics";
+import { auth } from './app/firebase.js';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
