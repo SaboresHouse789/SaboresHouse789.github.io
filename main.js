4417f78b4afb4188d5f43d338b13133c8596d43a
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 02:20:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Código existente...
-
+import './app/firebase.js';
 // Después de que se haya completado el registro exitosamente
 // Puedes agregar esto dentro de la función donde se realiza el registro
 // o en el lugar adecuado según tu implementación.
@@ -10,7 +10,6 @@ import './app/googleLogin.js';
 import './app/facebookLogin.js';
 import './app/githubLogin.js';
 import './app/logout.js';
-import './app/firebase.js';
 import './app/postList.js';
 // Aquí se muestra un ejemplo básico usando Firebase Auth
 auth.createUserWithEmailAndPassword(email, password)
