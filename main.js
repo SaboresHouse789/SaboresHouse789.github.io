5248fde15f568026cf241aae632e78498dc37aeb
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 01:05:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+const { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 
