d90aa5ba76f3cc83b0d0f25973c7305bf0679b5a
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 19:26:23 -0600

    Update signupForm.js

--- app/signupForm.js
+++ app/signupForm.js
@@ -16,33 +16,4 @@ console.log(userCredentials)
   } catch(error) {
     console.log(error)
    
-  
 
-
-
-
-  
-    // Close the signup modal
-    const signupModal = document.querySelector('#signupModal');
-    const modal = bootstrap.Modal.getInstance(signupModal);
-    modal.hide();
-
-    // reset the form
-    signUpForm.reset();
-
-    // show welcome message
-    showMessage("Welcome" + userCredentials.user.email);
-
-  } catch (error) {
-    if (error.code === 'auth/email-already-in-use') {
-      showMessage("Email already in use", "error")
-    } else if (error.code === 'auth/invalid-email') {
-      showMessage("Invalid email", "error")
-    } else if (error.code === 'auth/weak-password') {
-      showMessage("Weak password", "error")
-    } else if (error.code) {
-      showMessage("Something went wrong", "error")
-    }
-  }
-
-});
