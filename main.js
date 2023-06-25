c8afb644ad668dc47130a934e79978571a29ccc2
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 20:20:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+// Código existente...
+
+// Después de que se haya completado el registro exitosamente
+// Puedes agregar esto dentro de la función donde se realiza el registro
+// o en el lugar adecuado según tu implementación.
+
+// Aquí se muestra un ejemplo básico usando Firebase Auth
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Registro exitoso
+    // Realizar acciones adicionales si es necesario
+
+    // Recargar la página después de 1 segundo
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  })
+  .catch((error) => {
+    // Manejar errores de registro
+    console.log(error);
+  });
+
+// Código existente...
+
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
 import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 import { loginCheck } from "./app/loginCheck.js";
 import { setupPosts } from "./app/postList.js";
+
 import './app/signupForm.js';
 import './app/signinForm.js';
 import './app/googleLogin.js';
@@ -8,8 +47,6 @@ import './app/facebookLogin.js';
 import './app/githubLogin.js';
 import './app/logout.js';
 import './app/postList.js';
-
-// Resto del código existente...
 
 // list for auth state changes
 onAuthStateChanged(auth, async (user) => {
