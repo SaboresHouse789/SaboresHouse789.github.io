9a160d225f5c5f1d71a79c5bb4668e257b7a802c
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-24 19:34:01 -0600

    Update signupForm.js

--- app/signupForm.js
+++ app/signupForm.js
@@ -1,5 +1,6 @@
 import { createUserWhitEmailAndPasswword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 import { auth } from './firebase.js';
+
 const signUpForm = document.querySelector("#signup-form");
 
 signUpForm.addEventListener("submit", async (e) => {
