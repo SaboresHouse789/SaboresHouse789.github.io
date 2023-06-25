f88a342bca5e52a555ff3ff5fb9d557c72f8bd05
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-25 01:58:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "./app/firebase.js";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
+  authDomain: "sabores-house.firebaseapp.com",
+  databaseURL: "https://sabores-house-default-rtdb.firebaseio.com",
+  projectId: "sabores-house",
+  storageBucket: "sabores-house.appspot.com",
+  messagingSenderId: "42245565592",
+  appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
+  measurementId: "G-87B5870XV6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Código existente...
 import { firebase} from './app/firebase.js';
 // Después de que se haya completado el registro exitosamente
