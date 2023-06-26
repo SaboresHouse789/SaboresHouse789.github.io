314e16796046084c7a9b094e69151409819a5fb9
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-26 10:57:42 +0000

    si

--- app/loginCheck.js
+++
@@ -1,9 +0,0 @@
-export const loginCheck = (user) => {
-  if (user) {
-    loggedInLinks.forEach((link) => (link.style.display = "block"));
-    loggedOutLinks.forEach((link) => (link.style.display = "none"));
-  } else {
-    loggedInLinks.forEach((link) => (link.style.display = "none"));
-    loggedOutLinks.forEach((link) => (link.style.display = "block"));
-  }
-};
