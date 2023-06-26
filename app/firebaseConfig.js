025336d753e754a7757741c6f32036fe90a4db96
SaboresHouse789 <136309220+SaboresHouse789@users.noreply.github.com>
2023-06-26 10:19:45 +0000

    333

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,11 +1,19 @@
 const firebaseConfig = {
     apiKey: "AIzaSyDnhtgMB_BZ6siMX2ti_54Jt3t9AdOBKIs",
+
     authDomain: "sabores-house.firebaseapp.com",
+
     databaseURL: "https://sabores-house-default-rtdb.firebaseio.com",
+
     projectId: "sabores-house",
+
     storageBucket: "sabores-house.appspot.com",
+
     messagingSenderId: "42245565592",
+
     appId: "1:42245565592:web:9bf287743dc96c8c3734ed",
+
     measurementId: "G-87B5870XV6"
+
 };
 
